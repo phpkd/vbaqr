b2e75dcde8c61a6897fc0877946d7c26d34ff9d6
Omranic <me@omranic.com>
2010-03-06 11:44:47 +0000

    Updating MD5 hash!!

--- upload/includes/md5_sums_phpkd_vbaqr.php
+++ upload/includes/md5_sums_phpkd_vbaqr.php
@@ -17,10 +17,10 @@
 
 $md5_sums = array(
 	'/clientscript' => array(
-		'phpkd_vbaqr.js' => '4498557d7ef32431b0bafb4a58303e47',
+		'phpkd_vbaqr.js' => '1a402c30eaf809aeb4432cd9f6ff8693',
 	),
 	'/includes/xml' => array(
-		'bitfield_phpkd_vbaqr.xml' => '4498557d7ef32431b0bafb4a58303e47',
+		'bitfield_phpkd_vbaqr.xml' => 'ad5ba726edaea1a1f88913dae9dbc503',
 	),
 );
 $md5_sum_softwareid = 'phpkd_vbaqr';
