9fad506a21d69e7d5022f3342a8db216259c8378
Omranic <me@omranic.com>
2010-03-23 10:34:50 +0000

    1. Updated feature list in both 'info.txt' & 'contribution.txt'. 2. Updated history in both 'info.txt' & 'contribution.txt'. 3. Updated product description in product's file all other places. 4. Updated MD5 hash. 5. Added missing phrases to the product file. 6. Added missing svn:keywords to some files.

--- upload/includes/md5_sums_phpkd_vbaqr.php
+++ upload/includes/md5_sums_phpkd_vbaqr.php
@@ -17,10 +17,10 @@
 
 $md5_sums = array(
 	'/clientscript' => array(
-		'phpkd_vbaqr.js' => '1a402c30eaf809aeb4432cd9f6ff8693',
+		'phpkd_vbaqr.js' => 'fe8fb56b6eee7eb18886873aee064b42',
 	),
 	'/includes/xml' => array(
-		'bitfield_phpkd_vbaqr.xml' => 'ad5ba726edaea1a1f88913dae9dbc503',
+		'bitfield_phpkd_vbaqr.xml' => '311b1a748dae604ca8685e9909e0c9f2',
 	),
 );
 $md5_sum_softwareid = 'phpkd_vbaqr';
